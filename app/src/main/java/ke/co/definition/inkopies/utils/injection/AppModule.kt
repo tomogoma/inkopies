e5a123dbacf17ed7322019e3d553dc20c11cc044
tomogoma <ogomatom@gmail.com>
2018-03-07 23:34:15 +0300

    Refactor out unused import

--- app/src/main/java/ke/co/definition/inkopies/utils/injection/AppModule.kt
+++ app/src/main/java/ke/co/definition/inkopies/utils/injection/AppModule.kt
@@ -5,7 +5,6 @@ import dagger.Module
 import dagger.Provides
 import ke.co.definition.inkopies.model.ResourceManager
 import ke.co.definition.inkopies.model.ResourceManagerImpl
-import javax.inject.Inject
 import javax.inject.Singleton
 
 /**
